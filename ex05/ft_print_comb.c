8e2c078d0f5ec73c28e908caa1d1d99b3999d78d
Aidos Orynbay <aorynbay@lab1r5s14.42abudhabi.ae>
2024-05-01 17:26:42 +0400

    modified comb.c

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -1,35 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aorynbay <aorynbay@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 16:57:06 by aorynbay          #+#    #+#             */
+/*   Updated: 2024/05/01 17:18:52 by aorynbay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
-#include <stdio.h>
 
-int main()
+void	ft_print_comb(void)
 {
-    int a = 0;
-    int b = 0;
-    int c = 0;
+	char	a;
+	char	b;
+	char	c;
+	char	comma_space[];
 
-    while(c < 10 && a< 10 && b < 10)
-    {
+	a = 48;
+	b = 48;
+	c = 48;
+	comma_space = ", ";
+	while (c < 58 && a < 58 && b < 58)
+	{
+		c++;
+		if (c == 58)
+		{
+			b++;
+			c = 48;
+		}
+		if (b == 58)
+		{
+			a++;
+			b = 48;
+			c = 48;
+		}
+		if (a != b && a != c && b != c)
+		{
+			write(1, &a, 1);
+			write(1, &b, 1);
+			write(1, &c, 1);
+			write(1, comma_space, 1);
+		}
+	}
+}
 
-        c++;
-        if (c == 10)
-        {
-            b++;
-            c = 0;
-        }
-        if (b == 10)
-        {
-            a++;
-            b = 0;
-            c = 0;
-        }
-
-        if(a != b && a != c && b != c)
-        {
-            printf("%d", a);
-            printf("%d", b);
-            printf("%d, ", c);
-        }
-    }
-    return 0;
-
+int	main(void)
+{
+	ft_print_comb();
+	return (0);
 }
